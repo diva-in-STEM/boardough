88acaf5186756dea0f7f1fc372d34abb90439ec1
Reuben <rggiyfe@protonmail.com>
2025-07-12 12:54:15 +0100

    Added initial homepage layout and create dashboard modal.

--- app.py
+++ app.py
@@ -2,9 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def homepage():
-    return render_template('index.html')
+@app.route("/")
+@app.route("/index")
+def index():
+	dashboards = [
+		{
+			'name': 'Dashboard One',
+			'details': 'Source: api.app.com'
+        },
+		{
+			'name': 'Dashboard Two',
+			'details': 'Source: api.game.com'
+        },
+    ]
+	return render_template("index.html", dashboards=dashboards)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port='5000')
+	app.run(debug=True)
